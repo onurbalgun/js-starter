a99c6233f7faaacb2568990a18ea3ca423260d23
onurbalgun <onur.balgun@gmail.com>
2022-10-25 01:54:46 +0300

    file structrue change

---
+++ demo/oop3.js
@@ -0,0 +1,7 @@
+
+export class Logger{
+    log(title,data)
+    {
+        console.log("Logger: ",title,data);
+    }
+}
---
+++ demo/products.js
@@ -0,0 +1,18 @@
+//ürünleri alın
+let products = [
+	{unitPrice: 100, productName: "Ürün 1", discount: true, rate: 15},
+	{unitPrice: 150, productName: "Ürün 2", discount: false, rate: 0},
+	{unitPrice: 200, productName: "Ürün 3", discount: true, rate: 2.5},
+];
+
+
+products.pop()
+//isme gore filtreleme fonksiyonu
+//isme gore ilke bulunan productu getirme fonksiyonu
+// unitPrice<Parametre getirme fonksiyonu(array)
+// unitPrice>Parametre getirme fonksiyonu(array)
+//indirimdeki ürünleri getirme
+//ürün ekleme
+//verilen isim ile ürün silme
+//tüm ürünleri getirme
+
